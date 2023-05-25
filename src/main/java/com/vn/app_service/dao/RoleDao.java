001cdc318e19289f1150772fc1fd3f7ad536cbc5
Nguyen Xuan Kien <kien.nguyenxuan@vti.vn>
2023-05-25 11:14:58 +0700

    update base 25/05/2023

--- src/main/java/com/vn/app_service/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.vn.app_service.dao;
-
-import com.vn.app_service.entity.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleDao extends CrudRepository<Role, String> {
-}
---
+++ src/main/java/com/vn/app_service/model/ERole.java
@@ -0,0 +1,7 @@
+package com.vn.app_service.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
